f054add0b16c3b2460ec0f62c31e7d9bb12faeff
Tomer Becker <tbookra@gmail.com>
2024-12-03 12:00:40 +0200

    bento 6 elements grid

--- src/app/(landing)/tests/bento/page.tsx
+++ src/app/(landing)/tests/bento/page.tsx
@@ -9,21 +9,21 @@ const Page = () => {
   return (
     <div>
       <section className="relative py-24 sm:py-32 bg-brand-25">
-        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-20">
+        <MaxWidthWrapper className="flex flex-col items-center gap-8 sm:gap-20">
           <div>
             <h2 className="text-center text-base/7 font-semibold text-brand-600">
               Intuitive Monitoring
             </h2>
             <Heading>Stay ahead with real time insights</Heading>
           </div>
-          <div className="grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="grid gap-4 max-lg:grid-cols-1 max-lg:grid-rows-8 lg:grid-cols-4 lg:grid-rows-4">
             {/* first bento grid element */}
-            <div className="relative lg:row-span-2">
-              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]" />
+            <div className="relative lg:row-span-2 max-lg:row-start-1">
+              <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem] max-lg:rounded-t-[2rem]" />{/*max-lg:rounded-t-[2rem] */}
               <div className="relative flex flex-col h-full overflow-hidden rounded-[calc(theme(borderRadius.lg) + 1px)] lg:rounded-l-[calc(2rem+1px)]">
                 <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                   <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
-                    Real-time notifications
+                    first
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     Get notified about critical events the moment they happen,
@@ -41,15 +41,15 @@ const Page = () => {
                   </div>
                 </div>
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]" />
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]" />
             </div>
             {/* second bento grid element */}
-            <div className="relative max-lg:row-start-1">
-              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]" />
-              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+            <div className="relative max-lg:row-start-2">
+              <div className="absolute inset-px rounded-lg bg-white " />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[theme(borderRadius.lg)+1px]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                   <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
-                    Track Any event
+                    second
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     From new user signups to successful payments, PingPanda
@@ -66,7 +66,7 @@ const Page = () => {
                   />
                 </div>
               </div>
-              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]" />
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 " />
             </div>
             {/* third bento grid element */}
             <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
@@ -74,7 +74,7 @@ const Page = () => {
               <div className="relative flex h-full flex-col overflow-hidden rounded-[theme(borderRadius.lg)+1px]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                   <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
-                    Track Any properties
+                    third
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     Add any custom data you like to an event, such as a user
@@ -93,13 +93,63 @@ const Page = () => {
               </div>
               <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
             </div>
-            {/* forth bento grid element*/}
+            {/* forth bento grid element */}
+            <div className="relative max-lg:row-start-4">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]" />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    forth
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    From new user signups to successful payments, PingPanda
+                    notifies you for all critical events in your SaaS.
+                  </p>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                  <Image
+                    className="w-full max-lg:max-w-xs"
+                    src="/bento-any-event.png"
+                    alt="Bento box illustrating event tracking"
+                    width={500}
+                    height={300}
+                  />
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 " />
+            </div>
+            {/* fifth bento grid element */}
+            <div className="relative max-lg:row-start-5 lg:col-start-3 lg:row-start-2">
+              <div className="absolute inset-px rounded-lg bg-white " />
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[theme(borderRadius.lg)+1px]">
+                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    fifth
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Add any custom data you like to an event, such as a user
+                    email, a purchase amount or an exceeded quota.
+                  </p>
+                </div>
+                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                  <Image
+                    className="w-full max-lg:max-w-xs"
+                    src="/bento-custom-data.png"
+                    alt="Bento box illustrating custom data tracking"
+                    width={500}
+                    height={300}
+                  />
+                </div>
+              </div>
+              <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+            </div>
+            {/* last bento grid element*/}
             <div className="relative lg:row-span-2">
               <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] rounded-r-[2rem]" />
               <div className="relative flex h-full flex-col overflow-hidden rounded-[clac(theme(borderRadius.lg+1px))] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
                 <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                   <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
-                    Easy Integration
+                    last
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     Connect PingPanda with your existing workflows in minutes
